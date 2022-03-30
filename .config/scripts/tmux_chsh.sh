372ef4e6fc2ebaf37f4f5913dd208a5fc9078de4
alex <alexander@broocks.org>
2022-03-30 22:35:36 +0200

    fix paths

--- .config/scripts/tmux_chsh.sh
+++ .config/scripts/tmux_chsh.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # grabbed this one from theprimeagen, thx!
-selected=`cat ~/.config/scripts/.tmux-cht-languages ~/scripts/.tmux-cht-command | /home/alex/.fzf/bin/fzf`
+selected=`cat $HOME/.config/scripts/.tmux-cht-languages ~/scripts/.tmux-cht-command | $HOME/.fzf/bin/fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.config/scripts/.tmux-cht-languages; then
+if grep -qs "$selected" $HOME/.config/scripts/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
