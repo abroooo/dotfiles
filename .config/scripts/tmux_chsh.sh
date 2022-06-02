eb7be77b0b8ab6cd816ca2e7da3fa59a204ff440
Alexander BROOCKS <alexander.broocks@bachmann.info>
2022-06-02 08:07:33 +0200

    fix cht.sh keybinding, rn languages to topics and add git to topics

--- .config/scripts/tmux_chsh.sh
+++ .config/scripts/tmux_chsh.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # grabbed this one from theprimeagen, thx!
-selected=`cat $HOME/.config/scripts/.tmux-cht-languages ~/scripts/.tmux-cht-command | $HOME/.fzf/bin/fzf`
+selected=`cat $HOME/.config/scripts/.tmux-cht-topics ~/scripts/.tmux-cht-command | $HOME/.fzf/bin/fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" $HOME/.config/scripts/.tmux-cht-languages; then
+if grep -qs "$selected" $HOME/.config/scripts/.tmux-cht-topics; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
