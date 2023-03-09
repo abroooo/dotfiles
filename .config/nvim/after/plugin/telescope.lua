a5acb777a2c0ca3fe5be75204317800c89592df5
abroooo <spobro@gmx.net>
2023-03-09 11:41:57 +0000

    change telescope mapping for live_grep

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,7 +4,7 @@ vim.keymap.set('n', '<leader>a', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)  
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', 'C-a', builtin.live_grep, {})
 vim.keymap.set('n', ';', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>f;', ':lua require\'telescope.builtin\'.current_buffer_fuzzy_find()<CR>', {})
