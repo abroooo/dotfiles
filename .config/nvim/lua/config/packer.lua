c5e0ca7c246db3e3b285c9f4cb1de0b001ad389c
Alexander BROOCKS <alexander.broocks@bachmann.info>
2023-12-08 10:45:05 +0100

    nvim: add catppuccin and make it default

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -21,6 +21,7 @@ return require('packer').startup(function(use)
 	    end
     })
     use ( 'folke/tokyonight.nvim' )
+    use { "catppuccin/nvim", as = "catppuccin" }
     use { "folke/twilight.nvim"}
 
     use ( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} )
