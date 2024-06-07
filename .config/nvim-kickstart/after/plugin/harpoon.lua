cb3d74baf25ece4453ada2e923a017d21f2686f4
Alexander BROOCKS <alexander.broocks@bachmann.info>
2024-06-07 19:35:43 +0200

    add harpoon keybinds

--- .config/nvim-kickstart/after/plugin/harpoon.lua
+++ .config/nvim-kickstart/after/plugin/harpoon.lua
@@ -1,4 +1,8 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local mark = require 'harpoon.mark'
+local ui = require 'harpoon.ui'
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = '[H]arpoon add file', silent = true, noremap = true })
+vim.keymap.set('n', '<leader>hh', ui.toggle_quick_menu, { desc = '[H]arpoon [T]oggle menu', silent = true, noremap = true })
+vim.keymap.set('n', '<leader>hc', ui.toggle_quick_menu, { desc = '[H]arpoon clear all', silent = true, noremap = true })
+vim.keymap.set('n', '<leader>hn', ui.nav_next, { desc = '[H]arpoon [n]ext', silent = true, noremap = true })
+vim.keymap.set('n', '<leader>hp', ui.nav_prev, { desc = '[H]arpoon [p]revious', silent = true, noremap = true })
