5d67d06811e634562606eb3320225df927ed4ef8
Alexander BROOCKS <alexander.broocks@bachmann.info>
2024-06-21 07:23:05 +0200

    small changes

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -236,6 +236,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<M-h>', [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+vim.keymap.set('n', '<M-j>', [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+vim.keymap.set('n', '<M-k>', [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set('n', '<M-l>', [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
 -- Moving text
 -- -----------
 -- move selection up/down
