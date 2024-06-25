9a7e833afc658faa2c53d9f0db954e89e21b1073
Alexander BROOCKS <alexander.broocks@bachmann.info>
2024-06-25 12:19:03 +0200

    fix formatting

--- .config/nvim-kickstart/after/plugin/conform.lua
+++ .config/nvim-kickstart/after/plugin/conform.lua
@@ -1,27 +1,27 @@
-require('lazy').setup {
-  {
-    'stevearc/conform.nvim',
-    config = function()
-      require('conform').setup {
-        formatters_by_ft = {
-          markdown = { 'prettier' },
-          python = { 'black' },
-          lua = { 'stylua' },
-          rust = { 'rustfmt' },
-          cpp = { 'clang_format' },
-          c = { 'clang_format' },
-        },
-      }
-
-      -- Auto-format on save
-      vim.api.nvim_create_augroup('FormatOnSave', {})
-      vim.api.nvim_create_autocmd('BufWritePost', {
-        group = 'FormatOnSave',
-        pattern = { '*.md', '*.py', '*.lua', '*.rs', '*.cpp', '*.hpp' },
-        callback = function()
-          require('conform').format { async = true }
-        end,
-      })
-    end,
-  },
-}
+-- require('lazy').setup {
+--   {
+--     'stevearc/conform.nvim',
+--     config = function()
+--       require('conform').setup {
+--         formatters_by_ft = {
+--           markdown = { 'prettier' },
+--           python = { 'black' },
+--           lua = { 'stylua' },
+--           rust = { 'rustfmt' },
+--           cpp = { 'clang_format' },
+--           c = { 'clang_format' },
+--         },
+--       }
+-- 
+--       -- Auto-format on save
+--       vim.api.nvim_create_augroup('FormatOnSave', {})
+--       vim.api.nvim_create_autocmd('BufWritePost', {
+--         group = 'FormatOnSave',
+--         pattern = { '*.md', '*.py', '*.lua', '*.rs', '*.cpp', '*.hpp' },
+--         callback = function()
+--           require('conform').format { async = true }
+--         end,
+--       })
+--     end,
+--   },
+-- }
