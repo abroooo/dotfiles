899001096758373149642210665aa9cb337b4913
Alexander BROOCKS <alexander.broocks@bachmann.info>
2024-06-25 12:19:29 +0200

    keybind to switch header/source

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -650,6 +650,8 @@ require('lazy').setup({
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
+          map('<leader>ct', ':ClangSwitchSourceHeader<CR>', '[C]ode [T]oggle Header/Source')
+
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap.
           map('K', vim.lsp.buf.hover, 'Hover Documentation')
