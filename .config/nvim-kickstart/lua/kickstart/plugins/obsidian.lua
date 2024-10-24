b38a7f60678fbdcb0897293bbc0727ed45e36f52
abroooo <spobro@gmx.net>
2024-10-24 23:26:18 +0200

    restructure plugins

---
+++ .config/nvim-kickstart/lua/kickstart/plugins/obsidian.lua
@@ -0,0 +1,77 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = nil, -- recommended, use latest release instead of latest commit
+  -- version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = '*', -- * for always loading plugin
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+  --   "BufReadPre path/to/my-vault/**.md",
+  --   "BufNewFile path/to/my-vault/**.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = vim.env.NOTES,
+      },
+    },
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = 'telescope.nvim',
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      mappings = {
+        -- Create a new note from your query.
+        new = '<C-x>',
+        -- Insert a link to the selected note.
+        insert_link = '<C-l>',
+      },
+    },
+    -- disable ui because of render-markdown (only one plugin should be used for ui)
+    ui = { enable = false },
+    -- new notes should always go to inbox. From there I'll manually move them
+    notes_subdir = '0_inbox',
+    -- Where to put new notes. Valid options are
+    --  * "current_dir" - put new notes in same directory as the current buffer.
+    --  * "notes_subdir" - put new notes in the default notes subdirectory.
+    new_notes_location = 'notes_subdir',
+    -- Optional, boolean or a function that takes a filename and returns a boolean.
+    -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+    disable_frontmatter = true,
+    templates = {
+      folder = 'templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = 'journals',
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = '%Y-%m-%d',
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = '%B %-d, %Y',
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+    },
+    -- customize how note file names are generated given the ID, target directory, and title.
+    ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+    ---@return string|obsidian.Path The full path to the new note.
+    note_path_func = function(spec)
+      local path = spec.dir / tostring(spec.title)
+      return path:with_suffix '.md'
+    end,
+
+    -- see below for full list of options 👇
+  },
+}
