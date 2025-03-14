093db99bbd09bb53bf208380a567c22454e5bf59
abroooo <61380142+abroooo@users.noreply.github.com>
2025-03-14 16:52:40 +0100

    refactor install

--- install.sh
+++ install.sh
@@ -1,45 +1,161 @@
 #!/bin/bash
+# thx ghaith!
 
-# install nix
-curl -L https://nixos.org/nix/install | sh
+# Function to install packages on Arch Linux
+install_arch() {
+  sudo pacman -Syu --noconfirm git neovim curl bat eza starship zsh helix zellij alacritty python-pynvim nerd-fonts ripgrep fzf zoxide stow tmux
+}
 
-# source nix
-. ~/.nix-profile/etc/profile.d/nix.sh
+# Function to install packages on Ubuntu/Pop!_OS
+function install_ubuntu() {
 
-# install packages
-nix-env -iA \
-	nixpkgs.zsh \
-	nixpkgs.git \
-	nixpkgs.neovim \
-	nixpkgs.tmux \
-	nixpkgs.stow \
-	nixpkgs.fzf \
-	nixpkgs.ripgrep \
-	nixpkgs.bat \
-	nixpkgs.tldr \
-	nixpkgs.python3Full \
-	nixpkgs.lazygit \
-    nixpkgs.exa
+  sudo apt-get update
+  sudo apt-get install -y software-properties-common
+  sudo add-apt-repository ppa:neovim-ppa/stable
+  sudo apt-get update 
+  sudo apt-get install neovim
+  sudo apt-get install -y git curl build-essential bat zsh ripgrep fzf stow tmux
 
-# stow dotfiles
-stow -S .
+  # Install helix
+  sudo add-apt-repository ppa:maveonair/helix-editor
+  sudo apt update
+  sudo apt install helix
+  
+  # Install starship
+  curl -sS https://starship.rs/install.sh | sh
+ 
+  install_eza_apt
+  install_neovim
+  install_nerd_fonts
+}
 
-# add zsh as a login shell
-#command -v zsh | sudo tee -a /etc/shells
+function install_debian() {
+  sudo apt-get update
+  sudo apt-get install -y git curl build-essential bat zsh ripgrep fzf stow tmux
 
-# use zsh as default shell
-sudo chsh -s $(which zsh) $USER
+  # Install starship
+  curl -sS https://starship.rs/install.sh | sh
 
-# install neovim plugins
-nvim --headless +PlugInstall +qall
+  install_eza_apt
+  install_neovim
+  install_nerd_fonts
 
-echo "Installing vimplug"
-if ! test -f "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"; then
-    echo "$FILE exists."
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-else
-    echo "Vim Plug already installed"
+}
+
+function install_neovim() {
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage \
+  && chmod +x nvim-linux-x86_64.appimage && ./nvim-linux-x86_64.appimage --appimage-extract && cp -r squashfs-root/usr ~/.local/ \
+  && rm -rf nvim-linux-x86_64.appimage squashfs-root
+}
+
+function install_eza_apt() {
+  sudo apt-get update
+  sudo apt-get install -y gpg
+  sudo mkdir -p /etc/apt/keyrings
+  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+  sudo apt update
+  sudo apt install -y eza
+}
+ function install_dots(){
+     cd ~/dotfiles
+     stow -S .
+ }
+
+# Function to install packages on Fedora
+install_fedora() {
+  sudo dnf install -y git neovim python3-neovim curl alacritty zsh bat eza helix zoxide ripgrep fzf
+  install_nerd_fonts
+}
+
+# Function to install packages on Windows using winget
+install_windows() {
+  # Install Chocolatey
+  powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+
+  choco install git neovim starship nerd-fonts-hack nerd-fonts-fira-code nerd-fonts-jetbrains-mono nerd-fonts-iosevka -y
+
+}
+
+# Main function to run the installation and configuration
+function run() {
+  install
+  configure_shell
+}
+
+
+# Function to check and install necessary packages
+function install() {
+  # List of packages to check
+  packages=("zsh" "git" "nvim" "bat" "eza" "starship" "helix" "rg" "fzf" "zoxide")
+
+  # Check each package
+  all_installed=true
+  for package in "${packages[@]}"; do
+    if ! command -v "$package" &> /dev/null; then
+      echo "$package is not installed."
+      all_installed=false
+      break
+    fi
+  done
+
+  # Exit early if all packages are installed
+  if [ "$all_installed" = true ]; then
+    echo "All packages are already installed."
+    return
+  fi
+
+  # Ask the user for confirmation and exit if they decline
+  read -p "Do you want to install the packages? (Y/n): " confirm
+  if [[ "$confirm" =~ ^[Nn]$ ]]; then
+    echo "Installation aborted."
+    exit 1
+  fi
+
+  # Detect OS and call the appropriate function
+  if [[ -f /etc/arch-release ]]; then
+    install_arch
+  elif [[ -f /etc/lsb-release ]]; then
+    install_ubuntu
+  elif [[ -f /etc/debian_version ]]; then
+    install_debian
+  elif [[ -f /etc/fedora-release ]]; then
+    install_fedora
+  elif [[ "$OSTYPE" == "msys" ]]; then
+    install_windows
+  else
+    echo "Unsupported OS"
+    exit 1
+  fi
+
+}
+
+# Function to configure the shell to use zsh
+function configure_shell() {
+if [[ "$OSTYPE" != "msys" && "$OSTYPE" != "cygwin" ]]; then
+  if [[ "$SHELL" != *"zsh" ]]; then
+    chsh -s $(which zsh)
+  fi
 fi
+}
 
-exec zsh
+function install_nerd_fonts() {
+   # Path to check if the font is installed
+   font_path="$HOME/.local/share/fonts/NerdFonts"
+
+  # Check if the font is already installed
+  if [ -d "$font_path" ]; then
+    echo "Nerd fonts are already installed."
+    return
+  fi
+
+  # Install nerd-fonts manually
+  git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+  cd nerd-fonts
+  ./install.sh
+  cd ..
+  rm -rf nerd-fonts
+}
+# Start the installation process
+run
