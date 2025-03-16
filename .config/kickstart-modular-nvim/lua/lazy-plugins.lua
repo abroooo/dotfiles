019ac8bdad74f607a36ccb16830121381182e4ac
abroooo <61380142+abroooo@users.noreply.github.com>
2025-03-16 12:05:31 +0100

    add rustacean

--- .config/kickstart-modular-nvim/lua/lazy-plugins.lua
+++ .config/kickstart-modular-nvim/lua/lazy-plugins.lua
@@ -22,11 +22,32 @@ require('lazy').setup({
 
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
-  -- {
-  --   'mrcjkb/rustaceanvim',
-  --   version = '^5', -- Recommended
-  --   lazy = false, -- This plugin is already lazy
-  -- },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+      local mason_registry = require 'mason-registry'
+      local codelldb = mason_registry.get_package 'codelldb'
+      local extension_path = codelldb:get_install_path() .. '/extension'
+      local codelldb_path = extension_path .. 'adapter/codelldb'
+      local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
+      local cfg = require 'rustaceanvim.config'
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+          -- adapter = {
+          --   type = 'codelldb',
+          --   name = 'codelldb',
+          --   command = codelldb_path,
+          --   lldb = {
+          --     name = 'lldb',
+          --     lib = liblldb_path,
+          --   },
+        },
+      }
+    end,
+  },
 
   {
     'Bekaboo/dropbar.nvim',
