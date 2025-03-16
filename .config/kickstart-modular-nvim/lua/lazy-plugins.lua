12ee5064f0742141489d3745e84e64f968d3f4b6
abroooo <61380142+abroooo@users.noreply.github.com>
2025-03-16 12:05:00 +0100

    add dropbar

--- .config/kickstart-modular-nvim/lua/lazy-plugins.lua
+++ .config/kickstart-modular-nvim/lua/lazy-plugins.lua
@@ -29,6 +29,21 @@ require('lazy').setup({
   -- },
 
   {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+    },
+    config = function()
+      local dropbar_api = require 'dropbar.api'
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end,
+  },
+
+  {
     'sunaku/tmux-navigate',
     config = function()
       vim.g.tmux_navigator_no_mappings = 1 -- Disable default keybindings to avoid conflict with Neovim
