b74a47401e45b9fe3f1b1fb5a8e5d3e04b681dff
abroooo <61380142+abroooo@users.noreply.github.com>
2025-05-15 23:27:15 +0200

    simplify config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,21 +1,60 @@
 -- config based on https://github.com/KevinSilvester/wezterm-config
-local Config = require('config')
+-- local Config = require('config')
+--
+-- require('utils.backdrops')
+--    :set_files()
+--    -- :set_focus('#000000')
+--    :random()
+--
+-- require('events.right-status').setup()
+-- require('events.left-status').setup()
+-- require('events.tab-title').setup()
+-- require('events.new-tab-button').setup()
+-- require('utils.mouse')
+--
+-- return Config:init()
+--    :append(require('config.appearance'))
+--    :append(require('config.bindings'))
+--    :append(require('config.domains'))
+--    :append(require('config.fonts'))
+--    :append(require('config.general'))
+--    :append(require('config.launch')).options
 
-require('utils.backdrops')
-   :set_files()
-   -- :set_focus('#000000')
-   :random()
+-- TODO: remove old config and clean up here
+local wezterm = require('wezterm')
+local config = {}
 
-require('events.right-status').setup()
-require('events.left-status').setup()
-require('events.tab-title').setup()
-require('events.new-tab-button').setup()
-require('utils.mouse')
+-- font
+config.font = wezterm.font('JetBrains Mono', { weight = 'Regular' })
+config.font_size = 19.0
+config.line_height = 1.0
 
-return Config:init()
-   :append(require('config.appearance'))
-   :append(require('config.bindings'))
-   :append(require('config.domains'))
-   :append(require('config.fonts'))
-   :append(require('config.general'))
-   :append(require('config.launch')).options
+-- appearance
+config.color_scheme = 'tokyonight-storm'
+config.window_decorations = 'RESIZE'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+   left = 0,
+   right = 0,
+   top = 0,
+   bottom = 0,
+}
+-- TODO: custom command for disabling background image
+config.window_background_image = '/home/alex/.config/wezterm/backdrops/nord-space.png'
+config.window_background_image_hsb = {
+   brightness = 0.1,
+   -- hue = 40.0,
+   saturation = 1.0,
+}
+
+-- mouse
+config.mouse_bindings = {
+   {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'NONE',
+      action = wezterm.action({ CompleteSelectionOrOpenLinkAtMouseCursor = 'Clipboard' }),
+      -- NOTE: the default action is:
+      -- action=wezterm.action{CompleteSelectionOrOpenLinkAtMouseCursor="PrimarySelection"},
+   },
+}
+return config
