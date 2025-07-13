7f8f5e416caed73db944ae56a7e01bf15386734c
abroooo <61380142+abroooo@users.noreply.github.com>
2025-07-13 22:42:58 +0200

    match obsidian keybinds commands to new naming scheme

--- .config/kickstart-modular-nvim/after/plugin/obsidian.lua
+++ .config/kickstart-modular-nvim/after/plugin/obsidian.lua
@@ -1,7 +1,7 @@
 -- Obsidian
-require('lazy').load({plugins = {'obsidian.nvim'}})
-vim.keymap.set('n', '<leader>os', ':ObsidianQuickSwitch<CR>', { desc = '[O]bsidian file search', silent = true, noremap = true })
-vim.keymap.set('n', '<leader>on', ':ObsidianNew<CR>', { desc = '[O]bsidian new note', silent = true, noremap = true })
-vim.keymap.set('n', '<leader>og', ':ObsidianSearch<CR>', { desc = '[O]bsidian file grep', silent = true, noremap = true })
-vim.keymap.set('n', '<leader>od', ':ObsidianToday<CR>', { desc = '[O]bisian To[d]ay' })
-vim.keymap.set('n', '<leader>oy', ':ObsidianYesterday<CR>', { desc = '[O]bisian [Y]esterday' })
+require('lazy').load { plugins = { 'obsidian.nvim' } }
+vim.keymap.set('n', '<leader>os', ':Obsidian quick_switch<CR>', { desc = '[O]bsidian file search', silent = true, noremap = true })
+vim.keymap.set('n', '<leader>on', ':Obsidian new<CR>', { desc = '[O]bsidian new note', silent = true, noremap = true })
+vim.keymap.set('n', '<leader>og', ':Obsidian search<CR>', { desc = '[O]bsidian file grep', silent = true, noremap = true })
+vim.keymap.set('n', '<leader>od', ':Obsidian today<CR>', { desc = '[O]bisian To[d]ay' })
+vim.keymap.set('n', '<leader>oy', ':Obsidian yesterday<CR>', { desc = '[O]bisian [Y]esterday' })
